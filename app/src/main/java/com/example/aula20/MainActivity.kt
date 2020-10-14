3d638fad532815be63a6ad685b50d4e2f8ee3ca5
Abraão Facó <abraaofaco@gitlab.com>
2020-10-14 00:29:17 -0300

    Add Snackbar

--- app/src/main/java/com/example/aula20/MainActivity.kt
+++ app/src/main/java/com/example/aula20/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.aula20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun handleBtnLoginOnClick(v: View) {
+        showSnackbar("onClick BtnLogin")
+    }
+
+    fun showSnackbar(msg: String) {
+        val contextView = findViewById<View>(R.id.coordinator)
+
+        Snackbar.make(contextView, msg, Snackbar.LENGTH_SHORT)
+            .setAction("Ok") {
+                //OnClick
+            }.show()
     }
 }
